af817489c2a35847bf7409264fffb6f8646bc9ee
Rahul Verma <webgrowthhub@gmail.com>
2020-06-23 12:43:00 +0530

    ewqe

--- modules/register.js
+++ modules/register.js
@@ -1,5 +1,12 @@
 const mongoose= require('mongoose');
-mongoose.connect('mongodb://rahul:simonadmin@localhost:27017/emerald',{useNewUrlParser:true , useCreateIndex:true,useUnifiedTopology: true,});
+mongoose.connect('mongodb://rahul:simonadmin@localhost:27017/emerald',{useNewUrlParser:true , useCreateIndex:true,useUnifiedTopology: true,}).catch(error => handleError(error));
+
+// Or:
+try {
+  await mongoose.connect('mongodb://rahul:simonadmin@localhost:27017/emerald', { useNewUrlParser: true });
+} catch (error) {
+  handleError(error);
+}
 
 var conn =mongoose.Collection;
 
