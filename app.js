e53779959fed3e074dd6f251b71cacc0f9325382
Rahul Verma <webgrowthhub@gmail.com>
2020-06-23 12:53:41 +0530

    fdsF

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var mongoose = require("mongoose");
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var userModel=require("./modules/register");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-var userModel=require("./modules/register");
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
