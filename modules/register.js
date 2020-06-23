69a72bc97f5e9c1edf884efdae8eef1bd94f54a5
Rahul Verma <webgrowthhub@gmail.com>
2020-06-23 13:08:33 +0530

    fsadf

--- modules/register.js
+++ modules/register.js
@@ -1,5 +1,5 @@
 const mongoose= require('mongoose');
-mongoose.connect('mongodb://rahul:simonadmin@167.172.18.159:27017/emerald',{useNewUrlParser:true , useCreateIndex:true,useUnifiedTopology: true,});
+mongoose.connect('mongodb://rahul:simonadmin@localhost:27017/emerald',{useNewUrlParser:true , useCreateIndex:true,useUnifiedTopology: true,});
 
 var conn =mongoose.Collection;
 
